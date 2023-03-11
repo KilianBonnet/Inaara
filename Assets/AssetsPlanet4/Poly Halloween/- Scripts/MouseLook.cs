980fa3bc739db30d3118f6525307d3c5bd3c3feb
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-03-11 19:10:44 +0100

    Props #4

---
+++ Assets/AssetsPlanet4/Poly Halloween/- Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Polyperfect.Universal
+{
+    public class MouseLook : MonoBehaviour
+    {
+        public float mouseSensitivity = 3f;
+        public Transform playerBody;
+        float xRotation = 0f;
+
+
+
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        void Update()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
+  
---
+++ Assets/AssetsPlanet4/Poly Halloween/- Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace Polyperfect.Universal
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        public CharacterController controller;
+        public float speed = 12f;
+        public float gravity = -9.81f;
+        public float jumpHeight = 3f;
+
+        public Transform groundCheck;
+        public float groundDistance = 0.4f;
+        public LayerMask groundMask;
+
+
+        Vector3 velocity;
+        bool isGrounded;
+
+        // Update is called once per frame
+        void Update()
+        {
+            controller = GetComponent<CharacterController>();
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                controller.slopeLimit = 45.0f;
+                velocity.y = -2f;
+            }
+
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+            if (move.magnitude > 1)
+                move /= move.magnitude;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+
+            {
+                controller.slopeLimit = 100.0f;
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+
+
+
+
+        }
+    }
+}
