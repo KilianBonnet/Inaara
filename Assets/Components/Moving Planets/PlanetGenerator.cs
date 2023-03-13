c4419424c5272d63e8a458dd3d22edfce55ce744
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-03-13 18:50:49 +0100

    Adding moving planets #6

---
+++ Assets/Components/Moving Planets/PlanetGenerator.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlanetGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject[] planets;
+    [SerializeField] private float YSpawnRange = 50f;
+    [SerializeField] private float ZSpawnRange = 50f;
+    
+    private float timer;
+
+    private void Start()
+    {
+        timer = Random.Range(0, .5f);
+    }
+    
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            return;
+        }
+
+        int planetIndex = Random.Range(0, planets.Length);
+
+        Vector3 generatorPosition = transform.position;
+        float y = generatorPosition.y - YSpawnRange + Random.Range(0, YSpawnRange);
+        float z = generatorPosition.z - ZSpawnRange + Random.Range(0, ZSpawnRange);
+        Instantiate(planets[planetIndex], new Vector3(generatorPosition.x, y, z), new Quaternion());
+        
+        timer = Random.Range(2, 5);
+    }
+}
