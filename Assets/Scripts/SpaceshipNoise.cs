45302a0bea4c40a84c42eb46cf8d29ee9e9d17fe
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-03-13 22:48:14 +0100

    Rotation abandon #6

--- Assets/Scripts/SpaceshipNoise.cs
+++ Assets/Scripts/SpaceshipNoise.cs
@@ -4,40 +4,44 @@ using UnityEngine;
 
 public class SpaceshipNoise : MonoBehaviour
 {
-    /*[SerializeField] private float xRotationRange = 5;
-    [SerializeField] private float ZRotationRange = 5;*/
-    [SerializeField] private float xPositionRange = .2f;
-    [SerializeField] private float yPositionRange = .2f;
+    [SerializeField] private float xPositionRange = 1;
+    [SerializeField] private float yPositionRange = 2f;
     
     private Vector3 initialPosition;
-    private float moveSpeed;
+
+    private float positionSpeed;
     private Vector3 positionObjective;
+
+    private float rotationSpeed;
+    private Quaternion rotationObjective;
 
     private void Start()
     {
-        initialPosition = transform.position;
-        GenerateNewNoise();
+        Transform t = transform;
+        initialPosition = t.position;
+        GenerateNewPositionObjective();
     }
     
     private void Update()
     {
-        if(Vector3.Distance(transform.position, positionObjective) <= .1f) GenerateNewNoise();
+        if(Vector3.Distance(transform.position, positionObjective) <= .1f) GenerateNewPositionObjective();
         MoveToObjective();
     }
 
-    private void GenerateNewNoise()
+    private void GenerateNewPositionObjective()
     {
         positionObjective = new Vector3(
             initialPosition.x - xPositionRange + Random.Range(0f, 2 * xPositionRange),
             initialPosition.y - yPositionRange + Random.Range(0f, 2 * yPositionRange),
             initialPosition.z
         );
-        moveSpeed = Random.Range(.1f, .2f);
+
+        positionSpeed = Random.Range(.1f, .2f);
     }
 
     private void MoveToObjective()
     {
-        Vector3 v = positionObjective - transform.position;
-        transform.position +=  moveSpeed * Time.deltaTime * v.normalized;
+        Vector3 posDelta = positionObjective - transform.position;
+        transform.position +=  positionSpeed * Time.deltaTime * posDelta.normalized;
     }
 }
