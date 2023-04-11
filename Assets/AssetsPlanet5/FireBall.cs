1284d40eaf94b2ab8f7a033c6e9df77944669dbd
Ayoub <91192762+AyoubIMAMI@users.noreply.github.com>
2023-04-11 11:53:48 +0200

    FireBall #5

--- Assets/AssetsPlanet5/FireBall.cs
+++ Assets/AssetsPlanet5/FireBall.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float traveledDistance;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float toTravel = Time.deltaTime * Random.Range(10f, 20f);
+        traveledDistance += toTravel;
+        transform.position += Vector3.left * toTravel;
+        if (traveledDistance > 100) Destroy(this.gameObject);
     }
+
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Car")) return;
+        FindObjectOfType<ArcadeGameManager>().OnHit(other.gameObject);
+        Destroy(this.gameObject);
+    }*/
 }
